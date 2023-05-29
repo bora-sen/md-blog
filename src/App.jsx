5f17f46bea761bb1783e447109e38fa99fb16c8a
bora-sen <bahattinborasen@gmail.com>
2023-05-29 13:16:17 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function App() {
+  return <div className="bg-gray-700 w-full h-screen flex items-center justify-center font-bold text-7xl text-white">Initial Setup</div>
+}
+
+export default App
